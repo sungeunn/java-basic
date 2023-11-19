0dda21f55b6d578391ff79f0a2d3412aaf029199
sungeunn <saeun0512@gmail.com>
2023-11-19 22:25:46 +0900

    feat: 퀴즈 #1

---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,15 @@
+package chap_01;
+
+public class _Quiz_01 {
+
+    public static void main(String[] args) {
+        String busName = "상암08";
+        int time = 3;
+        float distance = 1.2F;
+
+        System.out.println(busName + "번 버스");
+        System.out.println("약 " + time + "분 후 도착");
+        System.out.println("남은 거리 " + distance + "km");
+    }
+
+}
