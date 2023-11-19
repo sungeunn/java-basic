6ed7a7c699454d70899717bf8c92fce32e476021
sungeunn <saeun0512@gmail.com>
2023-11-19 22:06:30 +0900

    feat: 상수

---
+++ src/chap_01/_06_Constants.java
@@ -0,0 +1,14 @@
+package chap_01;
+
+public class _06_Constants {
+
+    public static void main(String[] args) {
+        final String KR_COUNTRY_COME = "+82"; // 국가 번호
+//        KR_COUNTRY_COME = "+8282";
+        System.out.println(KR_COUNTRY_COME);
+
+        final double PI = 3.141592; // 원주율
+        final String DATE_OF_BIRTH = "2001-12-31"; // 생년 월일
+    }
+
+}
