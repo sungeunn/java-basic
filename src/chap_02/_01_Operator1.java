159a55088e0294ef9553733e953a5ce96644d446
sungeunn <saeun0512@gmail.com>
2023-11-20 23:38:40 +0900

    feat: 산술 연산자 (후반전)

--- src/chap_02/_01_Operator1.java
+++ src/chap_02/_01_Operator1.java
@@ -39,6 +39,35 @@ public class _01_Operator1 {
 
         c = a % b;
         System.out.println(c); // 0
+
+        // 증감 연산 ++, --
+        int val;
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(++val); // 11
+        System.out.println(val); // 11
+
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(val++); // 10
+        System.out.println(val); // 11
+
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(--val); // 9
+        System.out.println(val); // 9
+
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(val--); // 10
+        System.out.println(val); // 9
+
+        // 은행 대기번호 표
+        int waiting = 0;
+        System.out.println("대기 인원 : " + waiting++); // 대기 인원 : 0
+        System.out.println("대기 인원 : " + waiting++); // 대기 인원 : 1
+        System.out.println("대기 인원 : " + waiting++); // 대기 인원 : 2
+        System.out.println("총 대기 인원 : " + waiting); // 총 대기 인원 : 3
     }
 
 }
