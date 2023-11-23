7b0f90bfcefc4089bfa617790f74edd4f92ce2dd
sungeunn <saeun0512@gmail.com>
2023-11-23 23:49:47 +0900

    feat: 퀴즈 #3

---
+++ src/chap_03/_Quiz_03.java
@@ -0,0 +1,11 @@
+package chap_03;
+
+public class _Quiz_03 {
+
+    public static void main(String[] args) {
+        String birth = "901231-1234567";
+
+        System.out.println(birth.substring(0, 8));
+    }
+
+}
