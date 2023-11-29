6cd34aa8dc0814cab3859601776ad4191545b2db
sungeunn <saeun0512@gmail.com>
2023-11-29 23:39:03 +0900

    feat: Switch Case (후반전)

--- src/chap_04/_04_SwitchCase.java
+++ src/chap_04/_04_SwitchCase.java
@@ -11,7 +11,7 @@ public class _04_SwitchCase {
         // 3등 : 반액 장학금
         // 그 외 : 장학금 대상 아님
 
-        // If Else 문을 이용
+        // If Else 문을 이용 (여러 조건 또는 범위에 해당하는 조건)
         int ranking = 4; // 1등
         if (ranking == 1) {
             System.out.println("전액 장학금");
@@ -22,7 +22,7 @@ public class _04_SwitchCase {
         }
         System.out.println("조회 완료 #1");
 
-        // Switch Case 문을 이용
+        // Switch Case 문을 이용 (명확한 케이스가 있는 경우)
         ranking = 1;
         switch (ranking) {
             case 1:
@@ -53,6 +53,22 @@ public class _04_SwitchCase {
                 System.out.println("장학금 대상 아님");
         }
         System.out.println("조회 완료 #3");
+
+        // 중고상품의 등급에 따른 가격을 책정 (1급 : 최상, 4급 : 최하)
+        int grade = 1; // 등급
+        int price = 7000; // 기본 가격
+        switch (grade) {
+            case 1:
+                price += 1000; // price = price + 1000;
+            case 2:
+                price += 1000;
+            case 3:
+                price += 1000;
+                break;
+        }
+        System.out.println(grade + "등급 제품의 가격 : " + price + "원");
+        // 1등급 제품의 가격 : 10000원
+        // 2등급 제품의 가격 : 9000원
     }
 
 }
