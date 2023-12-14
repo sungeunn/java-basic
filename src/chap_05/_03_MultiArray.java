a89ce37a22f653f920b955321ae87e2119e6675a
sungeunn <saeun0512@gmail.com>
2023-12-14 23:49:46 +0900

    feat: 다차원 배열 (후반전)

--- src/chap_05/_03_MultiArray.java
+++ src/chap_05/_03_MultiArray.java
@@ -25,6 +25,22 @@ public class _03_MultiArray {
 
         // C5 에 접근하려면?
         System.out.println(seats[2][4]);
+
+        // 첫 줄에는 3칸, 둘째 줄에는 4칸, 셋째 줄에는 5칸
+        String[][] seats2 = {
+                {"A1", "A2", "A3"},
+                {"B1", "B2", "B3", "B4"},
+                {"C1", "C2", "C3", "C4", "C5"}
+        };
+
+        // A3 에 접근하려면?
+        System.out.println(seats2[0][2]);
+
+        // A5 에 접근하려면?
+        // System.out.println(seats2[0][4]);
+
+        // 3차원 배열 만들기 (세로 x 가로 x 높이)
+        String[][][] marray = new String[3][3][3];
     }
 
 }
